4b46d8186046f450a005bd7f0eb2555a8c5f17fe
Daniel <johnmejillon96@gmail.com>
2025-04-02 09:48:55 +0200

    feat(pages): exercise 4 solved

---
+++ pages/series-bonitas/[category]/[title].vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Categoría: aventuras</h1>
+    <h1>Título: {{ title }}</h1>
+  </div>
+</template>
+
+<script setup>
+const { title } = useRoute().params;
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/series-bonitas/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Estás en la página: Series Bonitas</h1>
+    <NuxtLink to="series-bonitas/aventuras"
+      >Visitar la categoría: aventuras</NuxtLink
+    >
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
